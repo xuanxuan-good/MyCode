c111eb249717cfe3f95c59eb95472fe7e91bc0d7
xuanxuan <57056325+xuanxuan-good@users.noreply.github.com>
2021-02-17 22:03:15 +0800

    Update 438.找到字符串中所有字母异位词.py

--- 之前做过的/438.找到字符串中所有字母异位词.py
+++ 之前做过的/438.找到字符串中所有字母异位词.py
@@ -7,27 +7,28 @@
 # @lc code=start
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # s: "cbaebabacd" p: "abc"
-        # 1.s从前到后，每次遍历p的长度，看是否是异位词，是就把索引加入
-        # 2.优化，map，出窗口的减掉，进窗口的加进来？看是不是异位词
-        '''
-        分别建立p和s[len(p)-1]的Counter;
-        滑动窗口的形式 len(p)-1开始，向后遍历s
-        s的对应索引的值 在Counter中+1
-        判断Counter相等，则是异位词；结果中加入起始的索引i-len(p)+1
-        加完之后，s对应位置的值-=1; 减完是0的再删除掉
-        返回res
-        '''
-        res = []
-        pCounter = collections.Counter(p)
-        sCounter = collections.Counter(s[:len(p)-1])
-        for i in range(len(p)-1, len(s)):
-            sCounter[s[i]] += 1
-            if sCounter == pCounter:
-                res.append(i-len(p)+1)
-            sCounter[s[i-len(p)+1]] -= 1
-            if sCounter[s[i-len(p)+1]] == 0:
-                del sCounter[s[i-len(p)+1]]
+        # 双指针，滑动窗口，同类型题目：76，567，剑指48
+        dictp = collections.Counter(p)
+        window = {}
+        left, right, res = 0, 0, []
+        vaild = 0
+        arrs = list(s)
+        while (right < len(arrs)):
+            c = arrs[right]
+            right += 1
+            if (c in dictp):
+                window[c] = window.get(c, 0) + 1
+                if dictp[c] == window[c]:
+                    vaild += 1
+            while (vaild == len(dictp.items())):
+                if (right - left == len(p)):
+                    res.append(left)
+                d = arrs[left]
+                left += 1
+                if (d in dictp):
+                    if dictp[d] == window[d]:
+                        vaild -= 1
+                    window[d] -= 1
         return res
 # @lc code=end
 
